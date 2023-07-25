fb0d1bbc5dc6f8e601100b1e5739b8a06a5f6df7
dupereira7 <126019441+dupereira7@users.noreply.github.com>
2023-07-25 14:40:16 -0300

    publicando o jogo

---
+++ app/sortear.Numero.js
@@ -0,0 +1,18 @@
+const menorValor = 1;
+const maiorValor = 1000;
+
+const numeroSecreto = gerarNumeroAleatorio();
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * maiorValor + 1);
+}
+
+console.log(`Número secreto: ${numeroSecreto}`)
+
+const elementoMenorValor = document.getElementById('valor-menor')
+
+elementoMenorValor.innerHTML = menorValor
+
+const elementoMaiorValor = document.getElementById('valor-maior')
+
+elementoMaiorValor.innerHTML = maiorValor
